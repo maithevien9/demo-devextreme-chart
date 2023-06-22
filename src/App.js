38746cb7b327af682a49d6f5d241030abb84a5ea
maithevien9 <vienthe.mai@team.enouvo.com>
2023-06-22 17:15:32 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import Chart, {
+  ArgumentAxis,
+  CommonSeriesSettings,
+  Format,
+  Grid,
+  Label,
+  Legend,
+  Margin,
+  Point,
+  ScrollBar,
+  Series,
+  Tooltip,
+  ValueAxis,
+  ZoomAndPan,
+} from 'devextreme-react/chart';
+import React from 'react';
+import keysData from './keys.json';
+import paramsData from './params.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ paddingTop: 200 }}>
+      <Chart palette='Violet' dataSource={paramsData.slice(0, 100)}>
+        <CommonSeriesSettings argumentField='frame' type='line' />
+
+        <ArgumentAxis>
+          <Grid visible={true} width={1300} />
+        </ArgumentAxis>
+
+        <ValueAxis>
+          <Grid visible={false} />
+        </ValueAxis>
+
+        {keysData.map((item) => (
+          <Series key={item.value} valueField={item.value} name={item.value}>
+            <Point visible={false} />
+          </Series>
+        ))}
+        <Margin bottom={20} />
+        <ArgumentAxis allowDecimals={false} axisDivisionFactor={60}>
+          <Label>
+            <Format type='decimal' />
+          </Label>
+          <Grid visible={true} />
+        </ArgumentAxis>
+        <Legend verticalAlignment='top' horizontalAlignment='right' />
+        <Tooltip enabled={true} />
+        <ZoomAndPan argumentAxis={'both'} dragToZoom={true} allowMouseWheel={true} panKey={'shift'} />
+        <ScrollBar visible={true} position='bottom' />
+      </Chart>
     </div>
   );
 }
