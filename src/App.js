73ea93b5c5e3005be81418dcaf65678a22a58ffc
maithevien9 <vienthe.mai@team.enouvo.com>
2023-06-26 17:00:16 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import lastData from './lastData.json';
 import paramsData from './params.json';
+import paramsData2 from './params2.json';
 
+import { useState } from 'react';
 import ReactEcharts from 'echarts-for-react';
 
-export default function App() {
-  const preventEventsHandler = () => {
-    console.log('prevent events handler called');
-  };
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+function Home() {
+  const [zoomLevel, setZoomLevel] = useState({
+    start: 20,
+    end: 40,
+  });
 
   const options = {
-    grid: {
-      left: '3%',
-      right: '4%',
-      bottom: '3%',
-      containLabel: true,
-    },
     toolbox: {
       feature: {
         dataZoom: {
           yAxisIndex: 'none',
         },
-        restore: {},
-        saveAsImage: {},
       },
     },
     xAxis: {
@@ -34,63 +32,147 @@ export default function App() {
     },
 
     series: paramsData.map((item) => ({ ...item, showSymbol: false })),
-    // dataZoom: [
-    //   {
-    //     type: 'inside',
-    //     xAxisIndex: 0,
-    //     filterMode: 'filter',
-    //   },
-    // ],
+
     dataZoom: [
-      // {
-      //   type: 'slider',
-      // },
+      {
+        type: 'slider',
+        showDetail: false,
+        zoomLock: false,
+        brushSelect: false,
+        bottom: 10,
+        height: 20,
+        ...zoomLevel,
+      },
       {
         type: 'inside',
       },
-      // {
-      //   show: true,
-      //   realtime: true,
-      //   start: 30,
-      //   end: 70,
-      //   xAxisIndex: [0, 1],
-      // },
-      // {
-      //   type: 'inside',
-      //   realtime: true,
-      //   start: 30,
-      //   end: 70,
-      //   xAxisIndex: [0, 1],
-      // },
     ],
-
-    // dataZoom: [
-    //   {
-    //     throttle: 50,
-    //     type: 'inside',
-    //   },
-    // ],
-    onClick: preventEventsHandler,
   };
 
-  preventEventsHandler(); // Test if it logs correctly
+  return <ReactEcharts option={options} style={{ height: 500 }} />;
+}
 
-  const onEvents = {
-    click: preventEventsHandler, // Note: The event name should be lowercase 'click'
-  };
+function About() {
+  const [startValue, setStartValue] = useState(0);
+  const [endValue, setEndValue] = useState(5);
 
-  const handleDataZoom = (params) => {
-    console.log('check');
+  const newArr = paramsData2.slice(startValue, endValue);
 
-    // setOptions(updatedOptions);
+  const options2 = {
+    toolbox: {
+      feature: {
+        dataZoom: {
+          yAxisIndex: 'none',
+        },
+      },
+    },
+    axisPointer: {
+      link: [
+        {
+          xAxisIndex: 'all',
+        },
+      ],
+    },
+    dataZoom: [
+      {
+        show: true,
+        realtime: true,
+        xAxisIndex: newArr.map((item, index) => index),
+        showDetail: false,
+        zoomLock: false,
+        brushSelect: false,
+        bottom: 0,
+        height: 20,
+      },
+      {
+        type: 'inside',
+        realtime: true,
+        start: 30,
+        end: 70,
+        xAxisIndex: newArr.map((item, index) => index),
+      },
+    ],
+    grid: newArr.map((item, index) => {
+      if (index) {
+        return { height: 150, top: 200 * index };
+      }
+      return { height: 150, top: 10 };
+    }),
+    xAxis: newArr.map((item, index) => {
+      if (index) {
+        return {
+          gridIndex: index,
+          type: 'category',
+          boundaryGap: false,
+          axisLine: { onZero: true },
+          data: lastData.map((item) => item.frame),
+        };
+      }
+      return { type: 'category', boundaryGap: false, axisLine: { onZero: true }, data: lastData.map((item) => item.frame) };
+    }),
+
+    yAxis: newArr.map((item, index) => {
+      if (index) {
+        return { type: 'value', gridIndex: index };
+      }
+      return { type: 'value', max: 500 };
+    }),
+    series: newArr.map((item, index) => ({ ...item, symbolSize: 8, xAxisIndex: index, yAxisIndex: index })),
   };
 
   return (
-    <ReactEcharts
-      option={options}
-      onEvents={{
-        dataZoom: handleDataZoom,
-      }}
-    />
+    <div>
+      <ReactEcharts option={options2} style={{ height: 200 * newArr.length }} />
+      <div style={{ margin: 20, display: 'flex', gap: 20, marginLeft: 300 }}>
+        <button
+          onClick={() => {
+            if (startValue >= 9) {
+              setEndValue((prev) => prev - 10);
+              setStartValue((prev) => prev - 10);
+            }
+          }}
+        >
+          Prev Page
+        </button>
+        <button
+          onClick={() => {
+            if (paramsData2.length > endValue + 10) {
+              setEndValue((prev) => prev + 10);
+              setStartValue((prev) => prev + 10);
+            }
+          }}
+        >
+          Next Page
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to='/'>Demo 1</Link>
+          </li>
+          <li>
+            <Link to='/demo2'>Demo 2</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/demo2'>
+            <About />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
