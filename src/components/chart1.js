a0c493ebc875d8f00f3e23d0b9ea842d314207e3
maithevien9 <vienthe.mai@team.enouvo.com>
2023-07-24 11:43:59 +0700

    update

--- src/components/chart1.js
+++ src/components/chart1.js
@@ -1,9 +1,11 @@
 import ReactEcharts from 'echarts-for-react';
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import '../App.css';
 import lastData from '../lastData.json';
 import paramsData from '../paramsHasAltitude.json';
 import paramsWithoutAltitude from '../paramsWithoutAltitude.json';
+import { Button, Modal, Input, Select, Space, Form, Radio, Checkbox } from 'antd';
+import { cloneDeep } from 'lodash-es';
 
 export default function Home() {
   const chartRef = useRef(null);
@@ -120,7 +122,189 @@ export default function Home() {
     ],
   };
 
+  const secondaryOptions = {
+    grid: { show: false },
+
+    xAxis: [
+      {
+        type: 'category',
+        data: newXAxis,
+        boundaryGap: false,
+        axisPointer: {
+          show: false,
+        },
+        splitLine: {
+          show: true, // Set to false to hide the default split lines
+        },
+      },
+      {
+        // type: 'category',
+        boundaryGap: false,
+        data: newXAxis2,
+        splitLine: {
+          show: false,
+        },
+        offset: 10,
+        axisTick: {
+          show: false, // Hide the xAxis tick line
+        },
+        axisLine: {
+          show: false, // Hide the xAxis line
+        },
+      },
+    ],
+    yAxis: {
+      type: 'value',
+      offset: 20,
+      splitLine: {
+        show: false,
+      },
+      // hide the yAxis line
+      show: false,
+    },
+    series,
+  };
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    form.submit();
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleAfterPrint = (value) => {
+    console.log('Print dialog closed trigged from button ?', value);
+  };
+
+  useEffect(() => {
+    window.addEventListener('afterprint', handleAfterPrint);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('afterprint', handleAfterPrint);
+    };
+  }, []);
+
+  const handlePrint = (value) => {
+    const elem = document.getElementById('componentB');
+    var domClone = elem.cloneNode(true);
+
+    let printSection;
+
+    printSection = document.createElement('div');
+    printSection.id = 'printSection';
+    document.body.appendChild(printSection);
+
+    printSection.appendChild(domClone);
+    setTimeout(() => window.print(), 500);
+
+    // const printContent = document.getElementById('componentB');
+    // if (printContent) {
+    //   const printWindow = window.open('', '_blank');
+    //   printWindow.document.write('<html><head><title>Print Preview</title>');
+    //   printWindow.document.write('<style type="text/css">@media print { @page { margin-top: 0; margin-bottom: 0; } }</style>');
+    //   printWindow.document.write('</head><body>');
+    //   printWindow.document.write('<div id="print-wrapper">' + printContent.innerHTML + '</div>');
+    //   printWindow.document.write('<script type="text/javascript">window.onafterprint = function() { window.close(); }</script>');
+    //   printWindow.document.write('</body></html>');
+    //   printWindow.document.close();
+    //   printWindow.print();
+    // }
+  };
+
+  const { Option } = Select;
+  const [form] = Form.useForm();
+
+  const [value, setValue] = useState(1);
+  const [checked, setChecked] = useState(false);
+
+  const onChange = (e) => {
+    console.log('radio checked', e.target.value);
+    setValue(e.target.value);
+  };
+
+  const onChangeCheckBox = (e) => {
+    setChecked(e.target.checked);
+  };
   return (
-    <ReactEcharts option={options} style={{ height: 540, paddingLeft: 50 }} ref={chartRef} opts={{ renderer: 'svg' }} key={chartKey} />
+    <div>
+      <ReactEcharts option={options} style={{ height: 540, paddingLeft: 50 }} ref={chartRef} opts={{ renderer: 'svg' }} key={chartKey} />
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: 200 }}>
+        <Button type='primary' onClick={showModal}>
+          Print
+        </Button>
+      </div>
+
+      <Modal title='Config Printing Area' open={isModalOpen} onOk={handleOk} onCancel={handleCancel} width={1300} okText='Print Option'>
+        <div style={{ display: 'flex' }}>
+          <div id='componentB'>
+            <ReactEcharts
+              option={secondaryOptions}
+              style={{ height: 540, paddingLeft: 50, width: 700 }}
+              ref={chartRef}
+              opts={{ renderer: 'svg' }}
+              key={chartKey}
+            />
+          </div>
+          <div style={{ width: 500 }}>
+            <Form
+              form={form}
+              onFinish={handlePrint}
+              initialValues={{
+                printer: 'pdf',
+                pageLayout: 'portrait',
+                pageSize: 'A4',
+              }}
+            >
+              <div style={{ display: 'flex' }}>
+                <div style={{ flex: 1 }}>
+                  <h2>Print Options</h2>
+                  <h2 style={{ marginBottom: 20 }}>Printing Area</h2>
+                  <Form.Item name='type'>
+                    <Radio.Group onChange={onChange} value={value}>
+                      <Space direction='vertical'>
+                        <Radio value={1}>Selected Area</Radio>
+                        <Radio value={2}>
+                          Range of Frame
+                          {value === 2 ? (
+                            <div style={{ display: 'flex', gap: 20 }}>
+                              <div>
+                                Start: <Input style={{ width: 100, marginLeft: 10 }} />
+                              </div>
+                              <div>
+                                End: <Input style={{ width: 100, marginLeft: 10 }} />
+                              </div>
+                            </div>
+                          ) : null}
+                        </Radio>
+
+                        <Radio value={3}>Frames on Screen</Radio>
+                        <Radio value={4}>All frame</Radio>
+                      </Space>
+                    </Radio.Group>
+                  </Form.Item>
+                  <h2 style={{ marginBottom: 20 }}>Printing Area</h2>
+                  <Checkbox value={checked} onChange={onChangeCheckBox}>
+                    Override Auto Trance Scaling
+                  </Checkbox>
+                  <div style={{ display: 'flex', gap: 20, alignItems: 'center', marginTop: 10 }}>
+                    <Input style={{ width: 200 }} disabled={!checked} />
+                    Page
+                  </div>
+                </div>
+              </div>
+            </Form>
+          </div>
+        </div>
+      </Modal>
+    </div>
   );
 }
