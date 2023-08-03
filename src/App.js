cbd21dedd6e1ccdda8a034aa2adcc4c03b240ccb
vienthe.mai <vienthe.mai@Admins-MacBook-Pro.local>
2023-08-03 17:47:33 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,13 +5,88 @@ import './App.css';
 import Home from './components/chart1';
 import lastData from './lastData.json';
 import paramsData2 from './params2.json';
+import Sroll from './components/Sroll';
+import { debounce } from 'lodash-es';
 
 // import paramsData from './params30h.json';
 import Chart3 from './components/charts3';
 import CheckAltitude from './components/checkAltitude.js';
 import styled from 'styled-components';
 
+const height = window.innerHeight - 250;
+const chartHeight = 240;
+const parametersMax = 15;
+
+const chartsOnSCreen = Math.floor(height / chartHeight);
+
+const Scroll = styled.div`
+  position: absolute;
+  top: 150px;
+  right: 10px;
+
+  header {
+    font-family: 'Lobster', cursive;
+    text-align: center;
+    font-size: 25px;
+  }
+
+  #info {
+    font-size: 18px;
+    color: #555;
+    text-align: center;
+    margin-bottom: 25px;
+  }
+
+  a {
+    color: #074e8c;
+  }
+
+  .scrollbar {
+    margin-left: 30px;
+    float: left;
+    height: ${height}px;
+    width: 105px;
+    background: #f5f5f5;
+    overflow-y: scroll;
+    margin-bottom: 25px;
+  }
+
+  .force-overflow {
+    min-height: ${(parametersMax * height) / 3}px;
+    background: white;
+  }
+
+  #wrapper {
+    text-align: center;
+    width: 500px;
+    margin: auto;
+  }
+
+  /*
+ *  STYLE 1
+ */
+
+  #style-1::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    border-radius: 10px;
+    background-color: white;
+  }
+
+  #style-1::-webkit-scrollbar {
+    width: 12px;
+    background-color: #f5f5f5;
+  }
+
+  #style-1::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    background-color: #555;
+  }
+`;
+
 const Wrapper = styled.div`
+  padding-top: 10;
+
   [fill='black'] {
     background: red !important;
     color: blue !important;
@@ -21,11 +96,26 @@ const Wrapper = styled.div`
 const bc = new BroadcastChannel('my-awesome-site');
 
 function About() {
+  const chartHeight = (height - 70 - (chartsOnSCreen - 1) * 100) / chartsOnSCreen;
+
+  const [position, setPosition] = useState({ start: 0, end: chartsOnSCreen });
+  const distanceParams = 100;
+  const [chartKey, setChartKey] = useState(0);
+
+  const debouncedPosition = debounce((value) => {
+    setPosition(value);
+    setChartKey((prev) => prev + 1);
+  }, 100);
+
   const [startValue, setStartValue] = useState(0);
-  const [endValue, setEndValue] = useState(9);
+  const [endValue, setEndValue] = useState(parametersMax);
   const chartRef = useRef(null);
 
-  const newArr = paramsData2.slice(startValue, endValue);
+  const patinatedParams = paramsData2.slice(startValue, endValue);
+
+  const newArr = patinatedParams.slice(position.start, position.end);
+
+  const lastIndex = chartsOnSCreen - 1;
 
   const handleChartClick = (params) => {
     if (params.componentType === 'markLine') {
@@ -74,44 +164,6 @@ function About() {
     xAxisIndex: index,
     yAxisIndex: index,
     showSymbol: false,
-    // markLine: {
-    //   data:
-    //     index === 8
-    //       ? [
-    //           {
-    //             label: {
-    //               formatter: '{b}',
-    //               backgroundColor: 'black',
-    //               borderRadius: 100,
-    //               color: 'black',
-    //               width: 10,
-    //               height: 10,
-    //               position: 'start',
-    //               distance: 26,
-    //             },
-    //             name: 'ii',
-    //             xAxis: '21632-3',
-    //           },
-    //           {
-    //             label: {
-    //               formatter: '{b}',
-    //               backgroundColor: 'red',
-    //               borderRadius: 100,
-    //               color: 'red',
-    //               width: 10,
-    //               height: 10,
-    //               position: 'start',
-    //               distance: 26,
-    //             },
-    //             name: 'ii',
-    //             xAxis: '21232-3',
-    //           },
-    //         ]
-    //       : [],
-    //   lineStyle: {
-    //     color: 'white',
-    //   },
-    // },
   }));
 
   const options2 = {
@@ -154,12 +206,12 @@ function About() {
     ],
     grid: newArr.map((item, index) => {
       if (index === 1) {
-        return { height: 150, top: 240 };
+        return { height: chartHeight, top: chartHeight + distanceParams };
       }
       if (index) {
-        return { height: 150, top: 220 * index };
+        return { height: chartHeight, top: (chartHeight + distanceParams) * index };
       }
-      return { height: 150, top: 30 };
+      return { height: chartHeight, top: 30 };
     }),
     xAxis: newArr.map((item, index) => {
       // if (index) {
@@ -170,8 +222,8 @@ function About() {
         data: lastData.map((item) => item.frame),
         type: 'category',
         axisLabel: {
-          color: index === 8 ? 'black' : 'white',
-          opacity: index === 8 ? 1 : 0,
+          color: index === lastIndex ? 'black' : 'white',
+          opacity: index === lastIndex ? 1 : 0,
         },
         axisPointer: {
           show: true,
@@ -208,7 +260,7 @@ function About() {
     graphic: series.map((item, index) => ({
       type: 'text',
       left: 150,
-      top: index ? (index === 1 ? 210 : 220 * index - 30) : 0, // Specify the y-coordinate (in pixels) of the text
+      top: index ? (index === 1 ? chartHeight + 60 : (chartHeight + 80) * index) : 0, // Specify the y-coordinate (in pixels) of the text
       style: {
         text: item.name, // The text you want to display
         fill: 'black', // The color of the text
@@ -217,15 +269,31 @@ function About() {
     })),
   };
 
+  // console.log({ startPosition, endPosition });
+
+  const handleScroll = (event) => {
+    const { scrollTop, scrollHeight, clientHeight } = event.target;
+    // const maxScrollTop = scrollHeight - clientHeight;
+    // const end = maxScrollTop - scrollTop;
+
+    debouncedPosition({
+      start: Math.floor((parametersMax * scrollTop) / scrollHeight),
+      end: Math.floor((parametersMax * scrollTop) / scrollHeight) + chartsOnSCreen,
+    });
+
+    // console.log('check', parametersMax * (1 - end / scrollHeight));
+  };
+
   return (
     <Wrapper>
-      <ReactEcharts option={options2} style={{ height: 200 * newArr.length + 190 }} opts={{ renderer: 'svg' }} ref={chartRef} />
+      <ReactEcharts option={options2} style={{ height: height }} opts={{ renderer: 'svg' }} ref={chartRef} key={chartKey} />
+      <Sroll />
 
       <div style={{ margin: 20, display: 'flex', gap: 20, marginLeft: 300 }}>
         <button
           style={{ height: 30 }}
           onClick={() => {
-            if (startValue >= 9) {
+            if (startValue >= parametersMax) {
               setEndValue((prev) => prev - 10);
               setStartValue((prev) => prev - 10);
             }
@@ -233,6 +301,13 @@ function About() {
         >
           Prev Page
         </button>
+
+        <Scroll>
+          <div className='scrollbar' id='style-1' onScroll={handleScroll}>
+            <div className='force-overflow'></div>
+          </div>
+        </Scroll>
+
         <button
           style={{ height: 30 }}
           onClick={() => {
